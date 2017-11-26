be120e75707ce4ef7b5dad4d29482fbee70db1d1
jazanne <bluelikejaz@gmail.com>
2017-11-26 11:46:20 -0500

    Fixes up tabs/spacing

--- search-engine-visibility.php
+++ search-engine-visibility.php
@@ -7,13 +7,12 @@ Version: 1.0
 Author URI: jazanne.com
 */
 
-function add_seo_css() {
-	if (!get_option('blog_public')){
-		wp_register_style( 'sev_admin_stylesheet', plugins_url( '/style.css', __FILE__ ) );
+add_action( 'admin_enqueue_scripts', 'sev_add_seo_css' );
+function sev_add_seo_css() {
+  if ( !get_option('blog_public') ){
+    wp_register_style( 'sev_admin_stylesheet', plugins_url( '/style.css', __FILE__ ) );
     wp_enqueue_style( 'sev_admin_stylesheet' );
-	}
+  }
 }
-
-add_action( 'admin_enqueue_scripts', 'add_seo_css' )
 
 ?>
